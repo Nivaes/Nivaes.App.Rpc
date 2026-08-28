3b26153ea9c57f3e776a015f69f219fe244a3750
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2026-08-28 14:33:46 +0200

    Test

---
+++ Sources/Nivaes.App.Rpc.Tests/Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nivaes.App.Rpc.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test()
+        {
+        }
+    }
+}
